79b776a86a0faefb3d8e12c0205fe757c1324833
Skillizen Learning Solutions Pte. Ltd <36956627+Skillizen@users.noreply.github.com>
2018-03-13 17:27:26 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<script type="text/javascript" src="https://gc.kis.scr.kaspersky-labs.com/02EC2405-2A45-414A-8A1C-AD3674CCCA36/main.js" charset="UTF-8"></script><script type="text/javascript">
+
+function Redirect() {
+   window.location="http://www.iskillsolympiad.org/";
+	 
+}
+setTimeout('Redirect()', 0);
+
+</script>
+</head>
+<body>
+
+</body>
+</html>
